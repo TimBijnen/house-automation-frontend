7bb412e3e4a7f9c3873e922d09718efe43a7ac8e
Tim Bijnen <>
2020-11-13 09:51:51 +0100

    Fix indentation

--- pages/index.js
+++ pages/index.js
@@ -16,61 +16,57 @@ export default function Home() {
     const socket = socketIOClient(ENDPOINT);
     
     useEffect( () => {
-      async function fetchData() {
-        const { data, error: e } = await RaspberryApi.getRpi();
-        // const { data, error: e } = await fetch("/api/rpi").then(r=>r.json());
-        setLoading( data ? false : true );
-        setError( e );
-        // debugger
-        // console.log(f)
-        setPins(data.pins);
-      }
+        async function fetchData() {
+            const { data, error: e } = await RaspberryApi.getRpi();
+            // const { data, error: e } = await fetch("/api/rpi").then(r=>r.json());
+            setLoading( data ? false : true );
+            setError( e );
+            // debugger
+            // console.log(f)
+            setPins(data.pins);
+        }
       socket.on("update", () => fetchData());
       fetchData();
     }, [] );
   
-  const toggle = async ( pin ) => {
-    const { data } = await RaspberryApi.togglePin(pin);
-    socket.emit("update");
-  }
-  
-  const toggleAll = async ( isActive ) => {
-    const { data } = await RaspberryApi.toggleAll( isActive );
-    socket.emit("update");
-  }
+    const toggle = async ( pin ) => {
+        const { data } = await RaspberryApi.togglePin(pin);
+        socket.emit("update");
+    }
+    
+    const toggleAll = async ( isActive ) => {
+        const { data } = await RaspberryApi.toggleAll( isActive );
+        socket.emit("update");
+    }
 
-  return (
-    <div data-testid="main-container" className="container">
-      <Head>
-        <title>lamPIe</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div data-testid="main-container" className="container">
+            <Head>
+                <title>lamPIe</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <h1 className="title">
-          lamPIe
-        </h1>
-        <Error error={ error } />
-        <Loader isShowing={ loading } />
-        { !error && pins.length > 0 && (
-          <Section title="TEST" pins={ pins } toggle={ toggle } toggleAll={ toggleAll } />
-        ) }
-      </main>
-      <footer>
-        
-      </footer>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-      `}</style>
-    </div>
-  )
+            <main>
+                <h1 className="title">
+                    lamPIe
+                </h1>
+                <Error error={ error } />
+                <Loader isShowing={ loading } />
+                { !error && pins.length > 0 && (
+                    <Section title="TEST" pins={ pins } toggle={ toggle } toggleAll={ toggleAll } />
+                ) }
+            </main>
+            
+            <style jsx global>{`
+            html,
+            body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                  sans-serif;
+            }
+            `}</style>
+        </div>
+    )
 }
